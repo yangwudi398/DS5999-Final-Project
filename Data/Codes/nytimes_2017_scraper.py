1f0dcb45dbec3cc9ad7035fd2764a8dae2d2f935
yangwudi398 <33925131+yangwudi398@users.noreply.github.com>
2019-04-29 15:06:04 -0400

    Update nytimes_2017_scraper.py

--- Data/Codes/nytimes_2017_scraper.py
+++ Data/Codes/nytimes_2017_scraper.py
@@ -70,6 +70,7 @@ def get_content(df_data):
     # Return the content table        
     return df_content
 
+
 # Get content for news in January 2018
 def in_jan(date):
     return date < "2017-02-01"
